e2dd95def30b37bad1bc732a35f9475052101a57
alexAlice <alex@alice.si>
2019-10-22 19:01:53 +0300

    initial app

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <form-wizard @on-complete="onComplete"
+      title=""
+      subtitle="" >
+      <tab-content title="Personal details"
+                    icon="ti-user">
+        My first tab content
+      </tab-content>
+      <tab-content title="Additional Info"
+                    icon="ti-settings">
+        My second tab content
+      </tab-content>
+      <tab-content title="Last step"
+                    icon="ti-check">
+        Yuhuuu! This seems pretty damn simple
+      </tab-content>
+    </form-wizard@on-complete="onComplete">
+  </div>
+</template>
+
+<script>
+import {FormWizard, TabContent} from 'vue-form-wizard'
+
+export default {
+  components: {
+    FormWizard,
+    TabContent
+  },
+  methods: {
+    onComplete() {
+      alert('Yay. Done!');
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import App from './App.vue'
+import VueFormWizard from 'vue-form-wizard'
+import 'vue-form-wizard/dist/vue-form-wizard.min.css'
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
+
+Vue.use(VueFormWizard)
